48851b9a4bde1a935494d78bbf6ca5b0c1e3b659
Manfly1 <aivanuyk0@gmail.com>
2022-01-21 13:22:35 +0200

    fix review

--- lib/states/create_card.rb
+++ lib/states/create_card.rb
@@ -1,7 +1,7 @@
 module States
   class CreateCard < Base
     def action
-      @cards = Factories::CardFactory.card(read_input_with_title(puts(I18n.t(:create_card_message))).to_sym)
+      @cards = Factories::CardFactory.card(read_input_with_title(I18n.t(:create_card_message)).to_sym)
       unless @cards
         I18n.t(:wrong_card_type_message)
         return
